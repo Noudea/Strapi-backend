a894caa41a9e308ad5a7c48a6f1a520ba71ed079
Nicolas Oudea <oudea.nicolas@gmail.com>
2021-02-03 10:36:43 +0100

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '80c0970c7241a6937e98737b602fde62'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '24ae5816-859c-4d79-bcff-0e1b0b39a45d'
+};
