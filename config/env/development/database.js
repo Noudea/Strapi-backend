76cc58a9726d3789ae0393667d511c8c7a0da20c
Nicolas Oudea <oudea.nicolas@gmail.com>
2021-02-03 12:08:19 +0100

    add notes

--- config/env/development/database.js
+++ config/env/development/database.js
@@ -4,17 +4,16 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        uri: env('DATABASE_URI')
-        // host: env('DATABASE_HOST', 'strapi-cluster.cugpx.mongodb.net'),
-        // srv: env.bool('DATABASE_SRV', true),
-        // port: env.int('DATABASE_PORT', 27017),
-        // database: env('DATABASE_NAME', 'backend'),
-        // username: env('DATABASE_USERNAME', 'admin'),
-        // password: env('DATABASE_PASSWORD', 'admin'),
+        host: env('DATABASE_HOST', 'strapi-cluster.cugpx.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'backend'),
+        username: env('DATABASE_USERNAME', 'admin'),
+        password: env('DATABASE_PASSWORD', 'admin'),
       },
       options: {
-        // authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl:  true //env.bool('DATABASE_SSL', true),
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl:  env.bool('DATABASE_SSL', true),
       },
     },
   },
